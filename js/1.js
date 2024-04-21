55e356f8b9d6dca2253c00e24ec87bcde08eb165
Sashkoe <revolver.bmx@gmail.com>
2024-04-21 20:30:31 +0200

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -120,70 +120,70 @@
 // function showProduct(product) {
 //   console.log(product);
 // }
-const scientists = [
-    {
-      name: "Albert",
-      surname: "Einstein",
-      born: 1879,
-      dead: 1955,
-      id: 1,
-    },
-    {
-      name: "Isaac",
-      surname: "Newton",
-      born: 1643,
-      dead: 1727,
-      id: 2,
-    },
-    {
-      name: "Galileo",
-      surname: "Galilei",
-      born: 1564,
-      dead: 1642,
-      id: 3,
-    },
-{
-      name: "Marie",
-      surname: "Curie",
-      born: 1867,
-      dead: 1934,
-      id: 4,
-    },
-    {
-      name: "Johannes",
-      surname: "Kepler",
-      born: 1571,
-      dead: 1630,
-      id: 5,
-    },
-    {
-      name: "Nicolaus",
-      surname: "Copernicus",
-      born: 1473,
-      dead: 1543,
-      id: 6,
-    },
-{
-      name: "Max",
-      surname: "Planck",
-      born: 1858,
-      dead: 1947,
-      id: 7,
-    },
-    {
-      name: "Katherine",
-      surname: "Blodgett",
-      born: 1898,
-      dead: 1979,
-      id: 8,
-    },
-    {
-      name: "Ada",
-      surname: "Lovelace",
-      born: 1815,
-      dead: 1852,
-      id: 9,
-    } ];
+// const scientists = [
+//     {
+//       name: "Albert",
+//       surname: "Einstein",
+//       born: 1879,
+//       dead: 1955,
+//       id: 1,
+//     },
+//     {
+//       name: "Isaac",
+//       surname: "Newton",
+//       born: 1643,
+//       dead: 1727,
+//       id: 2,
+//     },
+//     {
+//       name: "Galileo",
+//       surname: "Galilei",
+//       born: 1564,
+//       dead: 1642,
+//       id: 3,
+//     },
+// {
+//       name: "Marie",
+//       surname: "Curie",
+//       born: 1867,
+//       dead: 1934,
+//       id: 4,
+//     },
+//     {
+//       name: "Johannes",
+//       surname: "Kepler",
+//       born: 1571,
+//       dead: 1630,
+//       id: 5,
+//     },
+//     {
+//       name: "Nicolaus",
+//       surname: "Copernicus",
+//       born: 1473,
+//       dead: 1543,
+//       id: 6,
+//     },
+// {
+//       name: "Max",
+//       surname: "Planck",
+//       born: 1858,
+//       dead: 1947,
+//       id: 7,
+//     },
+//     {
+//       name: "Katherine",
+//       surname: "Blodgett",
+//       born: 1898,
+//       dead: 1979,
+//       id: 8,
+//     },
+//     {
+//       name: "Ada",
+//       surname: "Lovelace",
+//       born: 1815,
+//       dead: 1852,
+//       id: 9,
+//     } ];
     // 1)получити масив вчених які народились у 19ст;
     // const scient = scientists.filter(element => element.born >= 1800 && element.born < 1900 ).map(elements => `${elements.name} ${elements.surname}` );
     // console.log(scient);
@@ -221,71 +221,71 @@ const scientists = [
 // const lalala = new User("Max",33,333);
 // console.log(lalala.getInfo());
 // console.log(lalala);
-ЗАДАЧА 3
-Напиши клас Storage який створює об'єкти для керування складом товарів.
-При виклику отримуватиме один аргумент - початковий масив товарів, і записуватиме його у властивість items.
-Добавте методи класу:
+// ЗАДАЧА 3
+// Напиши клас Storage який створює об'єкти для керування складом товарів.
+// При виклику отримуватиме один аргумент - початковий масив товарів, і записуватиме його у властивість items.
+// Добавте методи класу:
 
-getItems() - повертає масив товарів.
-addItem(item) - отримує новий товар та додає його до поточних.
-removeItem(item) - отримає товар і, якщо він є, видаляє його з поточних.
+// getItems() - повертає масив товарів.
+// addItem(item) - отримує новий товар та додає його до поточних.
+// removeItem(item) - отримає товар і, якщо він є, видаляє його з поточних.
 
-const items = new Storage(["Виноград", "Банани", "Кокоси", "Ківі"]);
-items.addItem("Манго");
-items.removeItem("Ківі")
-ЗАДАЧА 4
-Напиши клас Client який створює об'єкт із властивостями login і email.
-Оголоси приватні властивості #login і #email, доступ до яких зроби через геттер та сеттер login і email.
+// const items = new Storage(["Виноград", "Банани", "Кокоси", "Ківі"]);
+// items.addItem("Манго");
+// items.removeItem("Ківі")
+// ЗАДАЧА 4
+// Напиши клас Client який створює об'єкт із властивостями login і email.
+// Оголоси приватні властивості #login і #email, доступ до яких зроби через геттер та сеттер login і email.
 
-ЗАДАЧА НА ЗАМИКАННЯ 5
-У вас є функція counter(), яка повинна повернути нове значення лічильника кожного разу, коли вона викликається. Проте, коли ви викликаєте counter() два рази, вона повертає одне і те ж значення. Ви хочете, щоб кожен виклик counter() повертав нове значення лічильника. Використовуючи замикання, напишіть функцію, яка розв'язує цю проблему.
-ЗАДАЧА 4
-Сортування примітивів.
-Виконати сортування масиву цін зі спадання та зростання.
-const prices = [2, 14, 1, 37, 26, 8];
+// ЗАДАЧА НА ЗАМИКАННЯ 5
+// У вас є функція counter(), яка повинна повернути нове значення лічильника кожного разу, коли вона викликається. Проте, коли ви викликаєте counter() два рази, вона повертає одне і те ж значення. Ви хочете, щоб кожен виклик counter() повертав нове значення лічильника. Використовуючи замикання, напишіть функцію, яка розв'язує цю проблему.
+// ЗАДАЧА 4
+// Сортування примітивів.
+// Виконати сортування масиву цін зі спадання та зростання.
+// const prices = [2, 14, 1, 37, 26, 8];
 
-ЗАДАЧА 5
-Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
-const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
-ЗАДАЧА 6
-Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
-Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
+// ЗАДАЧА 5
+// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+// const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+// ЗАДАЧА 6
+// Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
+// Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
 
-const courses = [
-  {
-    name: "Basic HTML+CSS",
-    topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
-  },
-  {
-    name: "Intermediate HTML+CSS",
-    topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
-  },
-  {
-    name: "Basic JavaScript",
-    topics: [
-      "VSCode",
-      "Type system",
-      "Loops",
-      "Functions",
-      "Conditions",
-      "Classes",
-      "DOM",
-      "Git",
-      "GitHub",
-    ],
-  },
-  {
-    name: "Intermediate JavaScript",
-    topics: [
-      "VSCode",
-      "NPM",
-      "Bundlers",
-      "Transpiling",
-      "Promises",
-      "AJAX",
-      "Git",
-      "GitHub",
-    ],
-  },
-];
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Functions",
+//       "Conditions",
+//       "Classes",
+//       "DOM",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Promises",
+//       "AJAX",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+// ];
 
